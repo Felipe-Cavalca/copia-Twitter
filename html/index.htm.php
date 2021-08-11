13f3a5e119b3ced904957a8b547c117d8bdf7267
Felipe-Cavalca <flpsnocvla@gmail.com>
2021-08-11 20:03:49 -0300

    Parte da Darla Adcionada

---
+++ html/index.htm.php
@@ -0,0 +1,79 @@
+<main class="logo col-5 ">
+        <section>
+            <div>
+                <div class="logo">
+                    <img src="img/ttw.JPG" alt="image" class="img-fluid">
+                </div>
+            </div>
+        </section>
+    </main>
+    
+   <main class="logopequeno">
+       <section class="row">
+            <div class="col-2" >
+                <figure class="text-center">
+                    <img src="img/ttwpequeno.JPG" alt="image" class="logo">
+                </figure>
+            </div>
+       </section>
+   </main>
+   <main class="escritagrande">
+        <section class="row">
+             <div class="col-12 text-white" >
+                <H1>Acontecendo agora</H1>
+             </div>
+        </section>
+    </main>    
+    <main class="escritamedia">
+        <section class="row">
+             <div class="col-12 text-white" >
+                <h2><strong>  Inscreva-se no Twitter hoje mesmo.</strong> </h2>
+             </div>
+        </section>
+    </main>   
+    
+   <!-- <main class="container">
+        <section class="row">
+            <div class="col-12">
+                <h2>Página 01</h2>
+            </div>
+            <div class="col-4">
+                <figure class="text-center">
+                    <img src="https://picsum.photos/601/601" alt="image" class="img-fluid rounded-circle">
+                    <figcaption>
+                        Legenda
+                    </figcaption>
+
+                </figure>
+            </div>
+            <div class="col-4">
+                <figure class="text-center">
+                    <img src="https://picsum.photos/599/599" alt="image" class="img-fluid rounded-circle">
+                    <figcaption>
+                        Legenda
+                    </figcaption>
+
+                </figure>
+            </div>
+            <div class="col-4">
+                <figure class="center">
+                    <img src="https://picsum.photos/600/600" alt="image" class="img-fluid rounded-circle">
+                    <figcaption>
+                        Legenda
+                    </figcaption>
+
+                </figure>
+            </div>
+        </section>
+        <section class="row">
+            <div class="col-12">
+                <h2>Texto</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam unde quod fuga, corporis voluptas laboriosam fugiat adipisci molestiae, ipsum natus accusamus fugit? Possimus nam totam fugiat id minima praesentium voluptate.</p>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias ab et odit ullam esse pariatur nisi ad numquam porro nam facilis sit, deleniti cumque placeat rem? Repudiandae esse dolores autem.</p>
+            </div>
+        </section>
+    </main>-->
+   <!--<footer class="container text-white">
+        <hr>
+        <span>Rodapé</span>
+    </footer>-->
